f937e29d8fd06ebb6a4dd1d350c233371bda313d
fahadulhassan <fahadulhassan2@gmail.com>
2024-03-02 11:01:16 +0500

    sadad

--- controller/appointmentController.js
+++ controller/appointmentController.js
@@ -63,7 +63,7 @@ export const handleCreateAppointment = async (req, res) => {
         let mail = MailGenerator.generate(response)
 
         let message = {
-            from: email,
+            from: "fahadulhassan2563@gmail.com",
             to: email,
             subject: "Patient Appointment",
             html: `<h1 style="color:#f2921d">mail</h1>
