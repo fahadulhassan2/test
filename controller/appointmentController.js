56eeeb8aa1d1424bf8e0891913086e470b14e1d9
fahadulhassan <fahadulhassan2@gmail.com>
2024-03-03 09:11:31 +0500

    aerwr

--- controller/appointmentController.js
+++ controller/appointmentController.js
@@ -66,7 +66,13 @@ export const handleCreateAppointment = async (req, res) => {
             from: "fahadulhassan2@gmail.com",
             to: email,
             subject: "Patient Appointment",
-            html: `<h1 style="color:#f2921d">mail</h1>
+            html: `
+        <style>
+        .test{
+           color: #f2921d ;
+        }
+        </style>
+            <h1 class="test">mail</h1>
                 <img src="https://zeba-clinic.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.9883db1f.png&w=384&q=75" alt="not"/>
             `
         }
